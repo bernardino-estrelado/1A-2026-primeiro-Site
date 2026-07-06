a1209a7d6a9ae1d526040a7ad3f65d54cd1f5f7c
bernardino-estrelado <bernardino.miguel@escola.pr.gov.br>
2026-07-06 11:53:10 -0300

    botão funcionou

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("curtidas")
+botao.addEventListener("click", aumentarCurtidas)
+
+function aumentarCurtidas(){
+    letcurtida= document.querySelector("span")
+    curtida.textContent++
+}
